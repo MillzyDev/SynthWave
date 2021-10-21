5ea63087ae7c08bf9b4c63693b2cd8cb30d6a706
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-10-21 16:33:13 +0100

    Create Response.java

---
+++ src/main/java/dev/millzyg/SynthWave/command/Response.java
@@ -0,0 +1,35 @@
+package dev.millzyg.SynthWave.command;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+
+import java.awt.*;
+
+public class Response {
+    private User author;
+    private String title, message, footer;
+    private Color colour;
+
+    public Response setAuthor(User author) { this.author = author; return this; }
+
+    public Response setTitle(String title) { this.title = title; return this; }
+
+    public Response setMessage(String message) { this.message = message; return this; }
+
+    public Response setFooter(String footer) { this.footer = footer; return this; }
+
+    public Response setColour(Color colour) { this.colour = colour; return this; }
+
+    public void sendResponse(TextChannel channel) {
+        EmbedBuilder eb = new EmbedBuilder();
+
+        if (author != null) eb.setAuthor(author.getName(), author.getAvatarUrl());
+        if (title != null) eb.setTitle(title);
+        if (message != null) eb.setDescription(message);
+        if (footer != null) eb.setFooter(footer);
+        if (colour != null) eb.setColor(colour);
+
+        channel.sendMessageEmbeds(eb.build()).queue();
+    }
+}
