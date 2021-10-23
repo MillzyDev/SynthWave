e5f032752d965374d8d2be8dcb8c784edde46ae4
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-10-23 10:25:21 +0100

    youtube api

--- src/main/java/dev/millzyg/SynthWave/Config.java
+++ src/main/java/dev/millzyg/SynthWave/Config.java
@@ -6,11 +6,13 @@ import java.util.Objects;
 
 public class Config {
     private String discordToken;
+    private String youTubeKey;
     private String prefix;
     public Config(){}
 
     public String getDiscordToken(){
         return this.discordToken;
     }
+    public String getYouTubeKey(){ return this.youTubeKey; }
     public String getPrefix() { return this.prefix; }
 }
