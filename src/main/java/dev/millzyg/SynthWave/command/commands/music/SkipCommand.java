a87b57bcc40fee6e3b4b5b75ab40c951c23b2510
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-10-23 10:26:09 +0100

    Create SkipCommand.java

---
+++ src/main/java/dev/millzyg/SynthWave/command/commands/music/SkipCommand.java
@@ -0,0 +1,51 @@
+package dev.millzyg.SynthWave.command.commands.music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import dev.millzyg.SynthWave.command.CommandContext;
+import dev.millzyg.SynthWave.command.ICommand;
+import dev.millzyg.SynthWave.command.Response;
+import dev.millzyg.SynthWave.music.GuildMusicManager;
+import dev.millzyg.SynthWave.music.PlayerManager;
+import dev.millzyg.SynthWave.music.TrackScheduler;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import javax.naming.AuthenticationException;
+import java.awt.*;
+import java.io.IOException;
+
+public class SkipCommand implements ICommand {
+    @Override
+    public void handle(CommandContext ctx) throws IOException, AuthenticationException {
+        TextChannel channel = ctx.getChannel();
+        PlayerManager playerManager = PlayerManager.getInstance();
+        GuildMusicManager guildMusicManager = playerManager.getGuildMusicManager(ctx.getGuild(), ctx.getChannel());
+        TrackScheduler scheduler = guildMusicManager.scheduler;
+        AudioPlayer player = guildMusicManager.player;
+
+        if (player.getPlayingTrack() == null) {
+            new Response()
+                    .setMessage("There's nothing to skip")
+                    .setColour(Color.RED)
+                    .sendResponse(channel);
+
+            return;
+        }
+
+        scheduler.nextTrack();
+
+        new Response()
+                .setColour(Color.YELLOW)
+                .setMessage("Skipped to the next song \uD83D\uDE0A")
+                .sendResponse(channel);
+    }
+
+    @Override
+    public String getName() {
+        return "skip";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Skips to the next track";
+    }
+}
